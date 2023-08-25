12356e7a29d9ac0f86ea992b2eba6f54ed50a84d
rafieldz <assumpcao.dev@gmail.com>
2023-08-24 23:26:52 -0300

    Add Categories

--- app/(dashboard)/[storeId]/layout.tsx
+++ app/(dashboard)/[storeId]/layout.tsx
@@ -3,6 +3,8 @@ import { auth } from "@clerk/nextjs";
 import { redirect } from "next/navigation";
 
 import Navbar from "@/components/navbar";
+
+
 export default async function DashboardLayout({
     children,
     params
@@ -24,11 +26,14 @@ export default async function DashboardLayout({
 
     if (!store) {
         redirect('/')
+
     }
     return (
         <>
             <Navbar />
             {children}
         </>
+
+
     )
 }
